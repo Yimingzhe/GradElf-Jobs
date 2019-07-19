7dadfddeeeb6ffc836d0051cc2fd60f68a85d12b
异名者 <1398947887@qq.com>
2019-07-19 22:34:32 +0800

    Update

--- databases/users.js
+++ databases/users.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var mongoDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/users';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mongoDB, {useNewUrlParser: true});
+mongoose.connect(mongoDB, {useMongoClient: true});//{useNewUrlParser: true}
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error!'));
