f4860cf95758ad673461eaf18e7836eed340e080
异名者 <1398947887@qq.com>
2019-07-19 23:11:31 +0800

    Update

---
+++ public/javascripts/base.js
@@ -0,0 +1,39 @@
+window.onload = function () {
+    // whether the browser is IE
+    var ieVersion = IEVersion();
+    if(ieVersion==-1 || ieVersion=="edge" || ieVersion>9){
+        // do nothing if it is not a low version of IE
+    }else{
+        alert("Hello! Your current browser version is too low, in order to view this website normally," +
+            " please change to another browser!");
+    }
+
+    function IEVersion() {
+        var userAgent = navigator.userAgent;
+        var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1;
+        var isEdge = userAgent.indexOf("Edge") > -1 && !isIE;
+        var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+        if(isIE) {
+            var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+            reIE.test(userAgent);
+            var fIEVersion = parseFloat(RegExp["$1"]);
+            if(fIEVersion == 7) {
+                return 7;
+            } else if(fIEVersion == 8) {
+                return 8;
+            } else if(fIEVersion == 9) {
+                return 9;
+            } else if(fIEVersion == 10) {
+                return 10;
+            } else {
+                return 6; // lower than IE7
+            }
+        } else if(isEdge) {
+            return 'edge';//edge
+        } else if(isIE11) {
+            return 11; //IE11
+        }else{
+            return -1;//not IE
+        }
+    }
+}
