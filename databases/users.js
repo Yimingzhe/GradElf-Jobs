3a1bfed3cde1a4ebfcae28f490791e44d0ee4120
异名者 <1398947887@qq.com>
2019-07-19 22:46:52 +0800

    Update

--- databases/users.js
+++ databases/users.js
@@ -1,8 +1,9 @@
 var mongoose = require('mongoose');
-var mongoDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/users';
+// var mongoDB = process.env.MONGODB_URI || 'mongodb://localhost:27017/users';
+var mongoDB = process.env.MONGODB_URI || 'mongodb://user:password1@ds351807.mlab.com:51807/heroku_f4rbc1tp';
 
 mongoose.Promise = global.Promise;
-mongoose.connect(mongoDB, {useMongoClient: true});//{useNewUrlParser: true}
+mongoose.connect(mongoDB, {useNewUrlParser: true});
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error!'));
